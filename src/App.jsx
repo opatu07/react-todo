65d95d8fa9a9c5752871bab2686bccb1b4fa89ea
ニシ <bunsekimal@gmail.com>
2025-05-26 17:12:41 +0900

    新規登録、表示

--- src/App.jsx
+++ src/App.jsx
@@ -1,117 +1,50 @@
 import { useState } from "react";
-import { nanoid } from "nanoid";
-import Todo from "./components/Todo";
-import Form from "./components/Form";
-import FilterButton from "./components/FilterButton";
 
-const FILTER_MAP = {
-  All: () => true,
-  Active: (task) => !task.completed,
-  Completed: (task) => task.completed,
-};
-const FILTER_NAMES = Object.keys(FILTER_MAP);
+function App() {
+  const [todo, setTodo] = useState("");
+  const [list, setList] = useState([]);
 
-function App(props) {
-
-  const [filter, setFilter] = useState("All");
-  const [searchTerm, setSearchTerm] = useState(""); 
-
-  const filterList = FILTER_NAMES.map((name) => (
-    <FilterButton 
-    key={name} 
-    name={name}
-    isPressed={name === filter}
-    setFilter={setFilter}
-    />
-  ));  
-
-  function addTask(name) {
-    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
-    setTasks([...tasks, newTask]);
+  function createTodo(event) {
+    setTodo(event.target.value);
   }
 
-  function toggleTaskCompleted(id) {
-    const updatedTasks = tasks.map((task) => {
-      // このタスクが編集されたタスクと同じ ID を持っている場合
-      if (id === task.id) {
-        // オブジェクトを開いて、 `completed` プロップが
-        // 反転された新しいオブジェクトを作成します。
-        return { ...task, completed: !task.completed };
-      }
-      return task;
-    });
-    setTasks(updatedTasks);
+  function addTodo(event) {
+    event.preventDefault();
+    if (todo.trim() !== "") {
+      setList([...list, todo]);
+      setTodo("");
+    }
   }
 
-  function deleteTask(id) {
-    const remainingTasks = tasks.filter((task) => id !== task.id);
-    setTasks(remainingTasks);
+  function editTodo(id) {
+    const editedTodo = list.find((index) => index === id);
+    setTodo(editedTodo);
   }
-
-  function editTask(id, newName) {
-    const editedTaskList = tasks.map((task) => {
-      // このタスクが編集されたタスクと同じIDを持っている場合
-      if (id === task.id) {
-        // タスクをコピーし、名前を更新する
-        return { ...task, name: newName };
-      }
-      // 編集されたタスクでない場合は、元のタスクを返します。
-      return task;
-    });
-    setTasks(editedTaskList);
-  }
-
-  const [tasks, setTasks] = useState(props.tasks);
-
-  const filteredTasks = tasks
-    .filter(FILTER_MAP[filter]) // ✅ フィルター適用（All, Active, Completed）
-    .filter((task) => {
-      const words = task.name.toLowerCase().split(/\s+/); // タスク名を単語に分割
-      return words.some(word => word.startsWith(searchTerm.toLowerCase())); // 先頭から比較
-    });
-
-  const taskList = filteredTasks
-  .map((task) => (
-    <Todo
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
-      toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask}
-      editTask={editTask}
-    />
-  ));
-
-  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
-  const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
   return (
-    <div className="todoapp stack-large">
-      <h1>Todo List</h1>
-      
-      <input
-        type="text"
-        placeholder="search keyword"
-        className="input input__lg"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-
-      <Form addTask={addTask} />
-
-      <div className="filters btn-group stack-exception">
-        {filterList}
+    <>
+      <div className="title">
+        <h1>Todo List</h1>
       </div>
-      <h2 id="list-heading">{headingText}</h2>
-      <ul
-        role="list"
-        className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading">
-        {taskList}
-      </ul>
-    </div>
+
+      <div className="add-title">
+        <h3>ADD TASK</h3>
+      </div>
+
+      <div className="todoapp">
+        <form onSubmit={addTodo}>
+          <input type="text" value={todo} onChange={createTodo} placeholder="New Task" />
+        </form>
+        <ul>
+          {list.map((item, index) => (
+            <li key={index}>
+              {item}
+              <button onClick={() => editTodo(index)}>Edit</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
-
 export default App;
